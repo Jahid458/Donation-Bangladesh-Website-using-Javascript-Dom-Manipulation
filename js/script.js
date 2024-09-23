a4c1ad954cf37255002849c8cc90973e7b9686c7
Jahid458 <jahidul898@outlook.com>
2024-09-23 16:14:13 +0600

    added  calculation card 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('hello')
+
+
+const calculateDonation = document.getElementById('calculte');
+calculateDonation.addEventListener('click', function(){
+  const donationAmount = parseFloat(document.getElementById("donation").value);
+  console.log( donationAmount) 
+
+  const totalDonateGiven = parseFloat(document.getElementById('total-donate').innerText);
+     totalDonate = donationAmount + totalDonateGiven
+     document.getElementById('total-donate').innerText = totalDonate; 
+    console.log(totalDonate)
+    
+
+    const balanceAmount = parseFloat(document.getElementById("balance-amount").innerText);
+    totalBalance = balanceAmount - donationAmount ; 
+    document.getElementById("balance-amount").innerText = totalBalance
+ 
+ 
+})
